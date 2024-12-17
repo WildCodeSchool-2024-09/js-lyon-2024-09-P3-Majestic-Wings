ae040e08d827e4e650d34e50babaea9ecc22bc57
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-12-17 17:11:07 +0100

    US01 terminée

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -4,12 +4,13 @@ import Header_Image from "../../../public/Header_Image.png";
 export default function Header() {
   return (
     <header className="header">
-      <div className="cover"></div>
-      <img
-        src={Header_Image}
-        alt="Image principale du site web, représentant un avion"
-        className="header-image"
-      />
+      <div className="cover">
+        <img
+          src={Header_Image}
+          alt="couverture principale du site web, représentant un avion"
+          className="header-image"
+        />
+      </div>
     </header>
   );
 }
