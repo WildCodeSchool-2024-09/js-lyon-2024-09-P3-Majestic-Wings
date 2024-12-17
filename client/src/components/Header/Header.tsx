96780c319fc70fc1bcb5026c4be5a3d0ba800481
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-12-17 17:01:38 +0100

    US01 terminée

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import Header_Image from "../../../public/Header_Image.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="cover"></div>
+      <img
+        src={Header_Image}
+        alt="Image principale du site web, représentant un avion"
+        className="header-image"
+      />
+    </header>
+  );
+}
