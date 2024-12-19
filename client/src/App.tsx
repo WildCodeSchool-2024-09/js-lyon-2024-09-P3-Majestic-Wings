cfaf8d2891c48b80f0d98c417c14f28b3fc9b478
TheophileSimoes <theophile.sdn@gmail.com>
2024-12-19 16:00:21 +0100

    Rajout du Login et des chemins

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import BurgerMenu from "./components/MenuBurger/MenuBurger";
-
+import Login from "./components/Login/Login";
 import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <>
       <nav className="NavBar">
+        <Login />
         <BurgerMenu />
       </nav>
       <Outlet />
