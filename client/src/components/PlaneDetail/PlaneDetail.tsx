d34f1d531a488389b9682fd6e843f34510a83ea6
Daniel Stacoffe <daniel.stacoffe@wanadoo.fr>
2025-01-28 13:37:43 +0100

    Page detail Avions

--- client/src/components/PlaneDetail/PlaneDetail.tsx
+++ client/src/components/PlaneDetail/PlaneDetail.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./PlaneDetail.css";
 
 interface PlaneProps {
   brand: string;
@@ -38,30 +39,35 @@ function PlaneDetail() {
 
   return (
     <div className="plane-detail">
-      <h1>
-        {planeDetail.brand} {planeDetail.model}
-      </h1>
-      <img
-        src={planeDetail.image}
-        alt={`${planeDetail.brand} ${planeDetail.model}`}
-      />
-      <img
-        src={planeDetail.photo}
-        alt={`${planeDetail.brand} ${planeDetail.model}`}
-      />
-      <img
-        src={planeDetail.plan}
-        alt={`${planeDetail.brand} ${planeDetail.model}`}
-      />
-      <ul>
-        <li> libelle : {planeDetail.libelle}</li>
-        <li> Autonomy Miles : {planeDetail.autonomy}</li>
-        <li> capacity Pers.: {planeDetail.capacity}</li>
-        <li> Speed MPH : {planeDetail.speed}</li>
-        <li> luggages nb : {planeDetail.luggages}</li>
-        <li> airport Names : {planeDetail.airports_names}</li>
-        <li> Country : {planeDetail.isocountry}</li>
-      </ul>
+      <div className="plane-photos">
+        <h1>
+          {planeDetail.brand} {planeDetail.model}
+        </h1>
+        <img
+          src={planeDetail.image}
+          alt={`${planeDetail.brand} ${planeDetail.model}`}
+        />
+        <img
+          src={planeDetail.photo}
+          alt={`${planeDetail.brand} ${planeDetail.model}`}
+        />
+        <img
+          src={planeDetail.plan}
+          alt={`${planeDetail.brand} ${planeDetail.model}`}
+        />
+      </div>
+      <div className="plane-caract">
+        <ul>
+          <li> libelle : {planeDetail.libelle}</li>
+          <li> Prestations : {planeDetail.cabine_prestations}</li>
+          <li> Autonomy Miles : {planeDetail.autonomy}</li>
+          <li> capacity Pers. : {planeDetail.capacity}</li>
+          <li> Speed MPH : {planeDetail.speed}</li>
+          <li> luggages nb : {planeDetail.luggages}</li>
+          <li> airport Names : {planeDetail.airports_names}</li>
+          <li> Country : {planeDetail.isocountry}</li>
+        </ul>
+      </div>
     </div>
   );
 }
