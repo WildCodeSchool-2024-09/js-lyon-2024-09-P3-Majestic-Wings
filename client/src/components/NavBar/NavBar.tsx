594fd6a6745efded0ca9d347a696431cdbeeb015
fouzy chikouche <fouzy_69@hotmail.fr>
2025-02-18 15:16:30 +0100

    security_password

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 import { useContext } from "react";
 import { toast } from "react-toastify";
+import CloseMenu from "../../../public/Croix.png";
 import OpenMenu from "../../../public/barre-blanc.png";
-import CloseMenu from "../../../public/close (1).png";
 import AuthContext from "../../Context/AuthContext";
 
 const NavBar = () => {
